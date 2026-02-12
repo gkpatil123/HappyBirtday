2d2513aabff9cd5a6ed75c87246ee58b06eaed21
gkpatil123 <kshirsagargajanan91@gmail.com>
2026-02-12 20:33:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Typing effect for greeting
-const greetingText = “I know I’m late, but I wanted to wish you in a special way.” 💖";
+const greetingText = “I know I’m late, but I wanted to wish you in a special way. 💖";
 const greetingElement = document.querySelector('.greeting');
 let charIndex = 0;
 
